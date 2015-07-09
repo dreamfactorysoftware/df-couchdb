f62a87dac88fb166cfad03f018920282e039542a
Arif Islam <arif@dreamfactory.com>
2015-07-09 15:28:23 -0400

    adding ServiceTypeGroups enum

--- src/Database/Seeds/DatabaseSeeder.php
+++ src/Database/Seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace DreamFactory\Core\CouchDb\Database\Seeds;
 use DreamFactory\Core\CouchDb\Models\CouchDbConfig;
 use DreamFactory\Core\CouchDb\Services\CouchDb;
 use DreamFactory\Core\Database\Seeds\BaseModelSeeder;
+use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\Models\ServiceType;
 
 class DatabaseSeeder extends BaseModelSeeder
@@ -17,7 +18,7 @@ class DatabaseSeeder extends BaseModelSeeder
             'config_handler' => CouchDbConfig::class,
             'label'          => 'CouchDB',
             'description'    => 'Database service for CouchDB connections.',
-            'group'          => 'Database',
+            'group'          => ServiceTypeGroups::DATABASE,
             'singleton'      => false,
         ]
     ];
