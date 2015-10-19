1264b7089ed15ee687e1deffef79cabdac12d9ae
Lee Hicks <leehicks@dreamfactory.com>
2015-10-19 10:58:07 -0400

    fix db designator

--- src/Models/CouchDbConfig.php
+++ src/Models/CouchDbConfig.php
@@ -23,7 +23,7 @@ class CouchDbConfig extends BaseServiceConfigModel
 
     protected $casts = ['options' => 'array'];
 
-    public static function validateConfig($config, $create=true)
+    public static function validateConfig($config, $create = true)
     {
         if ((null === ArrayUtils::get($config, 'dsn', null, true))) {
             if ((null === ArrayUtils::getDeep($config, 'options', 'db', null, true))) {
@@ -44,12 +44,20 @@ class CouchDbConfig extends BaseServiceConfigModel
         switch ($schema['name']) {
             case 'dsn':
                 $schema['label'] = 'Connection String';
-                $schema['default'] = 'http://localhost:5984';
-                $schema['description'] = 'The connection string for the service. The username, password, and db values can be added in the connection string or in the options below.';
+                $schema['default'] = 'http://username:password@localhost:5984/db';
+                $schema['description'] =
+                    'The connection string for the service. ' .
+                    'The username, password, and db values can be added in the connection string or in the options below.';
                 break;
             case 'options':
-                $schema['type'] = 'object(string,string)';
-                $schema['description'] = 'An array of options for the connection. Examples are username, password, and db name.';
+                $schema['type'] = 'object';
+                $schema['object'] =
+                    [
+                        'key'   => ['label' => 'Name', 'type' => 'string'],
+                        'value' => ['label' => 'Value', 'type' => 'string']
+                    ];
+                $schema['description'] =
+                    'An array of options for the connection, like db.';
                 break;
         }
     }
