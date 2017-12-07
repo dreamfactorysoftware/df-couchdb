795a8dc2470fc3d3e7341ec8a49175ac51712c6e
Lee Hicks <leehicks@dreamfactory.com>
2017-12-07 15:31:56 -0500

    Separating resources from resource handlers.

--- src/Services/CouchDb.php
+++ src/Services/CouchDb.php
@@ -3,9 +3,8 @@ namespace DreamFactory\Core\CouchDb\Services;
 
 use DreamFactory\Core\CouchDb\Database\Schema\Schema;
 use DreamFactory\Core\CouchDb\Resources\Table;
-use DreamFactory\Core\Exceptions\InternalServerErrorException;
-use DreamFactory\Core\Database\Resources\DbSchemaResource;
 use DreamFactory\Core\Database\Services\BaseDbService;
+use DreamFactory\Core\Exceptions\InternalServerErrorException;
 
 /**
  * CouchDb
@@ -15,35 +14,24 @@ use DreamFactory\Core\Database\Services\BaseDbService;
  */
 class CouchDb extends BaseDbService
 {
-    //*************************************************************************
-    //	Members
-    //*************************************************************************
-
-    /**
-     * @var array
-     */
-    protected static $resources = [
-        DbSchemaResource::RESOURCE_NAME => [
-            'name'       => DbSchemaResource::RESOURCE_NAME,
-            'class_name' => DbSchemaResource::class,
-            'label'      => 'Schema',
-        ],
-        Table::RESOURCE_NAME  => [
-            'name'       => Table::RESOURCE_NAME,
-            'class_name' => Table::class,
-            'label'      => 'Table',
-        ],
-    ];
-
-    //*************************************************************************
-    //	Methods
-    //*************************************************************************
-
     public function __construct($settings = [])
     {
         parent::__construct($settings);
 
         $this->setConfigBasedCachePrefix(array_get($this->config, 'db') . ':');
+    }
+
+    public function getResourceHandlers()
+    {
+        $handlers = parent::getResourceHandlers();
+
+        $handlers[Table::RESOURCE_NAME] = [
+            'name'       => Table::RESOURCE_NAME,
+            'class_name' => Table::class,
+            'label'      => 'Table',
+        ];
+
+        return $handlers;
     }
 
     protected function initializeConnection()
@@ -72,52 +60,5 @@ class CouchDb extends BaseDbService
         } catch (\Exception $ex) {
             throw new InternalServerErrorException("CouchDb Service Exception:\n{$ex->getMessage()}");
         }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAccessList()
-    {
-        $resources = [];
-
-//        $refresh = $this->request->queryBool( 'refresh' );
-
-        $name = DbSchemaResource::RESOURCE_NAME . '/';
-        $access = $this->getPermissions($name);
-        if (!empty($access)) {
-            $resources[] = $name;
-            $resources[] = $name . '*';
-        }
-
-        $result = $this->dbConn->listDatabases();
-        foreach ($result as $name) {
-            if ('_' != substr($name, 0, 1)) {
-                $name = DbSchemaResource::RESOURCE_NAME . '/' . $name;
-                $access = $this->getPermissions($name);
-                if (!empty($access)) {
-                    $resources[] = $name;
-                }
-            }
-        }
-
-        $name = Table::RESOURCE_NAME . '/';
-        $access = $this->getPermissions($name);
-        if (!empty($access)) {
-            $resources[] = $name;
-            $resources[] = $name . '*';
-        }
-
-        foreach ($result as $name) {
-            if ('_' != substr($name, 0, 1)) {
-                $name = Table::RESOURCE_NAME . '/' . $name;
-                $access = $this->getPermissions($name);
-                if (!empty($access)) {
-                    $resources[] = $name;
-                }
-            }
-        }
-
-        return $resources;
     }
 }
